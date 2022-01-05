bc22d2a48ae5163380f70f51abee74e1377989b6
huydq12 <huydq12@msb.com.vn>
2022-01-05 22:18:52 +0700

    first commit

---
+++ tf-setup/aws.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = "~> 1.0.9"
+  required_providers {
+    aws = {
+    source = "hashicorp/aws"
+    #  Lock version to prevent unexpected problems
+    version = "3.68"
+    }
+    null = {
+    source = "hashicorp/null"
+    version = "~> 3.1.0"
+    }
+    external = {
+    source = "hashicorp/external"
+    version = "~> 2.1.0"
+    }
+    kubernetes = {
+    source = "hashicorp/kubernetes"
+    version = "2.7.1"
+    }
+    
+  }
+}
+
+provider "aws" {
+  region                  = var.region
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = var.profile
+}
+provider "null" {}
+provider "external" {}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "az" {
+  state = "available"
+}
+
